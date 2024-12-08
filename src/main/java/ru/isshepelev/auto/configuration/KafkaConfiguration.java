00cdee3fc64e95c068e1e244a33f623d741ed30c
Ilya Shepelev <isshepelev2004@gmail.com>
2024-12-08 19:29:18 +0300

    kafka

--- src/main/java/ru/isshepelev/auto/configuration/KafkaConfiguration.java
+++ src/main/java/ru/isshepelev/auto/configuration/KafkaConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -40,14 +39,12 @@ public class KafkaConfiguration {
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps, new StringSerializer(), new JsonSerializer<>(objectMapper)));
     }
     @Bean
-    public ConsumerFactory<String, List<UUID>> orderDtoConsumerFactory(ObjectMapper objectMapper,
-                                                                       @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
+    public ConsumerFactory<String, List<UUID>> orderConsumerFactory(ObjectMapper objectMapper, @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); //localhost:9092
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "1");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, List.class.getName());
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(List.class, objectMapper, false));
@@ -56,7 +53,7 @@ public class KafkaConfiguration {
     public ConcurrentKafkaListenerContainerFactory<String, List<UUID>> kafkaListenerContainerFactory(ObjectMapper objectMapper,
                                                                                                      @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
         ConcurrentKafkaListenerContainerFactory<String, List<UUID>> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(orderDtoConsumerFactory(objectMapper,bootstrapServers));
+        factory.setConsumerFactory(orderConsumerFactory(objectMapper,bootstrapServers));
         return factory;
     }
 }
