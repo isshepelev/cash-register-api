0f7d76e2b22e8359443ab39f2b02691e85e69f00
Ilya Shepelev <isshepelev2004@gmail.com>
2025-02-19 20:53:58 +0300

    загрузка пользователей

--- src/main/java/ru/isshepelev/auto/security/service/DataLoader.java
+++ src/main/java/ru/isshepelev/auto/security/service/DataLoader.java
@@ -32,10 +32,30 @@ public class DataLoader implements CommandLineRunner {
         adminRole.setName("ROLE_ADMIN");
         roleRepository.save(adminRole);
 
-//        User admin = new User();
-//        admin.setUsername("admin");
-//        admin.setPassword(passwordEncoder.encode("admin"));
-//        admin.setRoles(Set.of(adminRole));
-//        userRepository.save(admin);
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+
+
+
+        user1.setEmail("1@1");
+        user1.setPassword(passwordEncoder.encode("1"));
+        user1.setUsername("1");
+        user1.setRoles(Set.of(adminRole));
+
+        user2.setEmail("2@2");
+        user2.setPassword(passwordEncoder.encode("2"));
+        user2.setUsername("2");
+        user2.setRoles(Set.of(adminRole));
+
+
+        user3.setEmail("3@3");
+        user3.setPassword(passwordEncoder.encode("3"));
+        user3.setUsername("3");
+        user3.setRoles(Set.of(adminRole));
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
     }
 }
