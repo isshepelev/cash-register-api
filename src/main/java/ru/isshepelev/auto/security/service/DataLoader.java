f6c46597a09675f5ecaee20d9f7d7cf56770a4e4
Ilya Shepelev <isshepelev2004@gmail.com>
2025-02-22 19:29:04 +0300

    Rename variable 'RoleUser' to 'Role'

--- src/main/java/ru/isshepelev/auto/security/service/DataLoader.java
+++ src/main/java/ru/isshepelev/auto/security/service/DataLoader.java
@@ -1,15 +1,12 @@
 package ru.isshepelev.auto.security.service;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import ru.isshepelev.auto.infrastructure.persistance.entity.Role;
-import ru.isshepelev.auto.infrastructure.persistance.repository.RoleRepository;
-import ru.isshepelev.auto.security.entity.RoleUser;
+import ru.isshepelev.auto.security.entity.Role;
 import ru.isshepelev.auto.security.entity.User;
-import ru.isshepelev.auto.security.repository.RoleUserRepository;
+import ru.isshepelev.auto.security.repository.RoleRepository;
 import ru.isshepelev.auto.security.repository.UserRepository;
 
 import java.util.Set;
@@ -18,17 +15,17 @@ import java.util.Set;
 @AllArgsConstructor
 public class DataLoader implements CommandLineRunner {
 
-    private final RoleUserRepository roleRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        RoleUser userRole = new RoleUser();
+        Role userRole = new Role();
         userRole.setName("ROLE_USER");
         roleRepository.save(userRole);
 
-        RoleUser adminRole = new RoleUser();
+        Role adminRole = new Role();
         adminRole.setName("ROLE_ADMIN");
         roleRepository.save(adminRole);
 
